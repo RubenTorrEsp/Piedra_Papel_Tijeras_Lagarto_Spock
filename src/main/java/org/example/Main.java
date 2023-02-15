83701c4e4a5c57f7168a29761d1b5deffa0f3055
Ruben <ruben.mte@gmail.com>
2023-02-15 20:25:19 +0100

    Limpieza de la clase Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static boolean jugarDeNuevo(){
+        boolean rejugar = false;
+        System.out.println("Si quiere volver a jugar, escriba \"si\"1");
+        Scanner scanner = new Scanner(System.in);
+        String respuesta = scanner.nextLine().toLowerCase();
+        if (respuesta.contains("si")) rejugar = true;
+        return rejugar;
+    }
+
     public static void main(String[] args) {
-        new Programa();
+        boolean otraPartida = true;
+        while (otraPartida) {
+            new Programa();
+            otraPartida = jugarDeNuevo();
+        }
     }
 
 }

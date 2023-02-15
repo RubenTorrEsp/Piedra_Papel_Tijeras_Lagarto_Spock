bea5483ce9995f1db56e520950c9ec41f23c77fd
Ruben <ruben.mte@gmail.com>
2023-02-15 20:58:50 +0100

    Añadir opcion salir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static boolean jugarDeNuevo(){
+    public static boolean jugarDeNuevo(String entrada){
         boolean rejugar = false;
-        System.out.println("Si quiere volver a jugar, escriba \"si\"");
-        Scanner scanner = new Scanner(System.in);
-        String respuesta = scanner.nextLine().toLowerCase();
-        if (respuesta.contains("si")) rejugar = true;
+        if(!entrada.contains("salir")) {
+            System.out.println("Si quiere volver a jugar, escriba \"si\"");
+            Scanner scanner = new Scanner(System.in);
+            String respuesta = scanner.nextLine().toLowerCase();
+            if (respuesta.contains("si")) rejugar = true;
+            else System.out.println("Gracias por jugar a Piedra, Papel, Tijeras.");
+        }
         return rejugar;
     }
 
     public static void main(String[] args) {
+        Programa programa;
         boolean otraPartida = true;
         while (otraPartida) {
-            new Programa();
-            otraPartida = jugarDeNuevo();
+            programa = new Programa();
+            otraPartida = jugarDeNuevo(programa.entrada);
         }
     }
 

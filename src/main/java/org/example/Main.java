e51070428e41cb1af920eb391534f196c1e56b75
Ruben <ruben.mte@gmail.com>
2023-02-15 20:41:47 +0100

    Limpieza de la clase Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static boolean jugarDeNuevo(){
         boolean rejugar = false;
-        System.out.println("Si quiere volver a jugar, escriba \"si\"1");
+        System.out.println("Si quiere volver a jugar, escriba \"si\"");
         Scanner scanner = new Scanner(System.in);
         String respuesta = scanner.nextLine().toLowerCase();
         if (respuesta.contains("si")) rejugar = true;
--- src/main/java/org/example/Programa.java
+++ src/main/java/org/example/Programa.java
@@ -11,7 +11,7 @@ public class Programa {
     String victoriaPiedra = "la piedra ROMPE a las tijeras";
     String victoriaTijeras = "las tijeras CORTAN al papel";
 
-    List<String> elementos = List.of(new String[]{"piedra", "papel", "tijeras"});
+    List<String> elementos = List.of(new String[]{"piedra", "papel", "tijeras", "salir", "1", "2", "3", "0"});
 
     Scanner scanner = new Scanner(System.in);
 
@@ -23,13 +23,13 @@ public class Programa {
     }
 
     public void darBienvenida() {
-        System.out.println("Bienvenido al juego Piedra, Papel, Tijeras!\nSeleccione su opción con nombre o número:");
-        System.out.println("1 - Piedra\n2 - Papel\n3 - Tijeras");
+        System.out.println("Bienvenido al juego Piedra, Papel, Tijeras!\nSeleccione su opción con nombre o número, o elija salir:");
+        System.out.println("1 - Piedra\n2 - Papel\n3 - Tijeras\n\n0 - Salir");
         System.out.println("-----------------------------------------------");
     }
 
     public String obtenerEntrada() {
-        entrada = scanner.nextLine().toLowerCase();
+        entrada = convertirNumeroEnElemento(scanner.nextLine().toLowerCase());
         System.out.println("Ha seleccionado: \""+entrada+"\"");
         return entrada;
     }
@@ -40,22 +40,32 @@ public class Programa {
         return seleccionJuego;
     }
 
+    public String convertirNumeroEnElemento(String entrada) {
+        String nuevaEntrada = entrada;
+        if (entrada.equals("1")) nuevaEntrada = "piedra";
+        if (entrada.equals("2")) nuevaEntrada = "papel";
+        if (entrada.equals("3")) nuevaEntrada = "tijeras";
+        if (entrada.equals("0")) nuevaEntrada = "salir";
+        return nuevaEntrada;
+    }
+
     public void comprobarVictoria() {
-        if(!elementos.contains(entrada)) System.out.println("El elemento seleccionado no es correcto. Ahora moriré.");
+        if(entrada.equals("salir")) System.out.println("Gracias por jugar a Piedra, Papel, Tijeras.");
+        else if(!elementos.contains(entrada)) System.out.println("El elemento seleccionado no es correcto. Ahora moriré.");
         else {
             System.out.println("El ordenador ha seleccionado \"" + seleccionJuego + "\"");
             if (seleccionJuego.contains(entrada)) System.out.println("Empate!!");
             else {
                 switch (entrada) {
-                    case "piedra":
+                    case "piedra","1":
                         if (seleccionJuego == "tijeras") System.out.println("¡¡¡Has ganado!!! Porque " + victoriaPiedra);
                         else System.out.println("Lo siento, has perdido, porque " + victoriaPapel);
                         break;
-                    case "papel":
+                    case "papel","2":
                         if (seleccionJuego == "piedra") System.out.println("¡¡¡Has ganado!!! Porque " + victoriaPapel);
                         else System.out.println("Lo siento, has perdido, porque " + victoriaTijeras);
                         break;
-                    case "tijeras":
+                    case "tijeras","3":
                         if (seleccionJuego == "papel") System.out.println("¡¡¡Has ganado!!! Porque " + victoriaTijeras);
                         else System.out.println("Lo siento, has perdido, porque " + victoriaPiedra);
                         break;

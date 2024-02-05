cc184c23986193b64f7ffb7d51a85ba76f049bc0
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-02-05 17:34:48 +0100

    Creación de la clase Textos

---
+++ src/main/java/org/example/Textos.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Textos {
+
+    String PiedraTijeras = "la piedra ROMPE las tijeras.";
+    String PiedraLagarto = "la piedra APLASTA al lagarto.";
+    String TijerasPapel = "las tijeras CORTAN el papel.";
+    String TijerasLagarto = "las tijeras DECAPITAN al lagarto.";
+    String PapelPiedra = "el papel ENVUELVE la piedra.";
+    String PapelSpock = "el papel DESAUTORIZA a Spock.";
+    String LagartoPapel = "el lagarto SE COME el papel.";
+    String LagartoSpock = "el lagarto ENVENENA a Spock.";
+    String SpockPiedra = "Spock DESINTEGRA la piedra.";
+    String SpockTijeras = "Spock DESMONTA las tijeras.";
+
+    String victoria = "Enhorabuena!! Has ganado, porque ";
+    String derrota = "Lo siento!! Has perdido, porque ";
+    String empate = "Empate!! Aprovecha el momento o vuelve a por más.";
+    
+}

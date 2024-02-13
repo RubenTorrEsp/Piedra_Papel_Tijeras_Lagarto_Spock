a0b83304c7fe3663f951cae3175355e6eabc52cb
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-02-13 12:49:18 +0100

    Modificacion de los nombres de los textos  de enfrentamiento

--- src/main/java/org/example/Textos.java
+++ src/main/java/org/example/Textos.java
@@ -5,16 +5,16 @@ public class Textos {
     static String bienvenida = "Bienvenido al juego Piedra, Papel, Tijeras, Lagarto, Spock!\nSeleccione su opción con nombre o número, o elija salir:";
     static String opciones = "1 - Piedra\n2 - Papel\n3 - Tijeras\n4 - Lagarto\n5 - Spock\n\n0 - Salir\n-----------------------------------------------";
 
-    static String PiedraTijeras = "la piedra ROMPE las tijeras.";
-    static String PiedraLagarto = "la piedra APLASTA al lagarto.";
-    static String TijerasPapel = "las tijeras CORTAN el papel.";
-    static String TijerasLagarto = "las tijeras DECAPITAN al lagarto.";
-    static String PapelPiedra = "el papel ENVUELVE la piedra.";
-    static String PapelSpock = "el papel DESAUTORIZA a Spock.";
-    static String LagartoPapel = "el lagarto SE COME el papel.";
-    static String LagartoSpock = "el lagarto ENVENENA a Spock.";
-    static String SpockPiedra = "Spock DESINTEGRA la piedra.";
-    static String SpockTijeras = "Spock DESMONTA las tijeras.";
+    static String piedra_tijeras = "la piedra ROMPE las tijeras.";
+    static String piedra_lagarto = "la piedra APLASTA al lagarto.";
+    static String tijeras_papel = "las tijeras CORTAN el papel.";
+    static String tijeras_lagarto = "las tijeras DECAPITAN al lagarto.";
+    static String papel_piedra = "el papel ENVUELVE la piedra.";
+    static String papel_spock = "el papel DESAUTORIZA a Spock.";
+    static String lagarto_papel = "el lagarto SE COME el papel.";
+    static String lagarto_spock = "el lagarto ENVENENA a Spock.";
+    static String spock_piedra = "Spock DESINTEGRA la piedra.";
+    static String spock_tijeras = "Spock DESMONTA las tijeras.";
 
     static String victoria = "Enhorabuena!! Has ganado, porque ";
     static String derrota = "Lo siento!! Has perdido, porque ";

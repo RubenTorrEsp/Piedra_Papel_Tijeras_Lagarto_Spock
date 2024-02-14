ea96075cb16731c17872c257e4b6ca24fa7c4ead
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-02-14 08:51:33 +0100

    Modificación de la clase ListasDeVictoria.  Ahora en vez de listas son mapas de listas

--- src/main/java/org/example/ListasDeVictoria.java
+++ src/main/java/org/example/ListasDeVictoria.java
@@ -1,14 +1,27 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import java.util.Arrays;
 
 public class ListasDeVictoria {
-    
-    static List<String> piedra = Arrays.asList("tijeras", "lagarto");
-    static List<String> papel = Arrays.asList("piedra", "spock");
-    static List<String> tijeras = Arrays.asList("papel", "lagarto");
-    static List<String> lagarto = Arrays.asList("spock", "papel");
-    static List<String> spock = Arrays.asList("tijeras", "piedra");
+
+    // Listas de elementos a los que vence cada uno
+    private static final Map<String, List<String>> listasVictoria = new HashMap<>();
+    static {
+        listasVictoria.put("piedra", Arrays.asList("tijeras", "lagarto"));
+        listasVictoria.put("papel", Arrays.asList("piedra", "spock"));
+        listasVictoria.put("tijeras", Arrays.asList("papel", "lagarto"));
+        listasVictoria.put("lagarto", Arrays.asList("spock", "papel"));
+        listasVictoria.put("spock", Arrays.asList("tijeras", "piedra"));
+    }
+
+    // Método que obtiene el elemento del jugador y el de la máquina y comprueba si el jugador gana o no.
+    public static Boolean obtenerVictoria(String elemJugador, String elemMaquina) {
+        Boolean victoria = false;
+        if(listasVictoria.get(elemJugador).contains(elemMaquina)) victoria = true;
+        return victoria;
+    }
 
 }

56385fcb09c9e8d964bfd0bb213d459a4995e0e7
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-02-22 12:00:26 +0100

    Arreglo del  archivo Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,28 +4,30 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void jugarDeNuevo(String entrada){
-        new Programa();
-        reJugar();
-    }
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         Programa.darBienvenida();
         jugarDeNuevo("");
     }
 
-    @SuppressWarnings("resource")
-    private static void reJugar(){
-        Scanner scanner = new Scanner(System.in);
+    public static void jugarDeNuevo(String entrada){
+        new Programa();
+        reJugar();
+    }
+
+    private static Boolean reJugar(){
+        Boolean reJugar = false;
         System.out.println("¿Desea volver a jugar? Escriba \"Si\" para jugar otra vez.");
         String elijeRejugar = scanner.nextLine().toLowerCase();
         switch (elijeRejugar){
             case "si":
+                reJugar = true;
                 System.out.println("Has elegido volver a jugar. Pronto estará disponible");
             default:
                 break;
         }
-
+        return reJugar;
     }
 
 }

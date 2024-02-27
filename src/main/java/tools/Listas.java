8b1ef23da136d2f274eaedec8892a549e2ad0be8
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-02-27 13:19:51 +0100

    Commit revertido

---
+++ src/main/java/tools/Listas.java
@@ -0,0 +1,43 @@
+package tools;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
+
+public class Listas {
+
+    // Lista de elementos aceptados
+    public static List<String> elementos = List.of(new String[]{
+        "piedra",
+        "papel",
+        "tijeras",
+        "spock",
+        "lagarto",
+        "salir",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "0"
+    });
+
+    // Listas de elementos a los que vence cada uno
+    private static final Map<String, List<String>> listasVictoria = new HashMap<>();
+    static {
+        listasVictoria.put("piedra", Arrays.asList("tijeras", "lagarto"));
+        listasVictoria.put("papel", Arrays.asList("piedra", "spock"));
+        listasVictoria.put("tijeras", Arrays.asList("papel", "lagarto"));
+        listasVictoria.put("lagarto", Arrays.asList("spock", "papel"));
+        listasVictoria.put("spock", Arrays.asList("tijeras", "piedra"));
+    }
+
+    // Método que obtiene el elemento del jugador y el de la máquina y comprueba si el jugador gana o no.
+    public static Boolean obtenerVictoria(String elemJugador, String elemMaquina) {
+        Boolean victoria = false;
+        if(listasVictoria.get(elemJugador).contains(elemMaquina)) victoria = true;
+        return victoria;
+    }
+
+}

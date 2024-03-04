338c8a6201fa3b644de9332ee718a636a7769064
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-03-04 07:59:35 +0100

    Correccion del Main para implementar el juego clasico

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println(Textos.bienvenida);
-        while(Programa.reJugar) new Programa();
+        while(ProgramaExtendido.reJugar) new ProgramaExtendido();
     }
 
 }

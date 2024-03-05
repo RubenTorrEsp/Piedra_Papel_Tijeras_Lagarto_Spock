be77031c141b25a1eac4cd27c4d691f190e65da6
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-03-05 10:59:07 +0100

    Limpieza  de codigo

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,6 +11,13 @@ public class Main {
 
     public static void inicio(){
         System.out.println(Textos.bienvenida);
+        //seleccion();
+    }
+
+    public static void seleccion(){
+        System.out.println("Elija a qué quiere jugar");
+        System.out.println("1- Juego extendido");
+        System.out.println("2- Juego clásico (Próximamente)");
     }
 
 }

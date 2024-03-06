0ce4d36fa7c50add83269758e015af72ec26d941
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-03-06 08:46:21 +0100

    Commit menu inicial con programa clasico

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,23 +1,39 @@
 package main;
 
 import tools.Textos;
+import java.util.Scanner;
 
 public class Main {
     
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         inicio();
-        while(ProgramaExtendido.reJugar) new ProgramaExtendido();
+        //while(ProgramaExtendido.reJugar) new ProgramaExtendido();
     }
 
     public static void inicio(){
         System.out.println(Textos.bienvenida);
-        //seleccion();
+        seleccion();
     }
 
+    //Comportamiento de prueba para no interferir en el main. A eliminar
     public static void seleccion(){
-        System.out.println("Elija a qué quiere jugar");
+        System.out.println("Elija a qué quiere jugar indicando el numero");
         System.out.println("1- Juego extendido");
         System.out.println("2- Juego clásico (Próximamente)");
+        String eleccion = scanner.nextLine();
+        switch (eleccion) {
+            case "1":
+                while(ProgramaExtendido.reJugar) new ProgramaExtendido();
+                break;
+            case "2":
+                System.out.println("Opcion no disponible en este momento. Disculpe las molestias");
+                break;
+            default:
+                System.out.println(Textos.seleccionInvalida);
+                break;
+        }
     }
 
 }

5de6ba69f58f61496499a2691198a3f9bf07998c
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-03-18 08:29:13 +0100

    Limpieza de codigo

--- src/main/java/main/User.java
+++ src/main/java/main/User.java
@@ -63,45 +63,6 @@ public class User {
 
     }
 
-
-
-    public static void leerArchivo(String nombreArchivo) {
-        try (BufferedReader br = new BufferedReader(new FileReader(archivoUsuarios));
-             PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(archivoUsuarios + ".temp")))) {
-            
-            while ((linea = br.readLine()) != null) {
-                // Dividir la línea en usuario y puntuación utilizando el punto y coma como delimitador
-                String[] partes = linea.split(";");
-                if (partes.length == 2) {
-                    // Establecer nombre y puntuacion del usuario
-                    nombre = partes[0];
-                    puntuacion = Integer.parseInt(partes[1]);
-
-                    System.out.println("El usuario "+nombre+" tiene una puntuacion de "+puntuacion+" puntos.");
-
-                    // Actualizar puntuacion
-                    //partes[1] = String.valueOf(actualizarPuntuacion(puntuacion));
-                    
-                    // Reconstruir la línea con las partes modificadas y escribir en el archivo temporal
-                    pw.println(partes[0] + ";" + partes[1]);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Renombrar el archivo temporal para reemplazar el archivo original
-        try {
-            java.nio.file.Files.move(
-                java.nio.file.Paths.get(archivoUsuarios + ".temp"),
-                java.nio.file.Paths.get(archivoUsuarios),
-                java.nio.file.StandardCopyOption.REPLACE_EXISTING
-            );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static Integer actualizarPuntuacion(Integer puntuacion){
         puntuacion++;
         return puntuacion;

33d711fca9cd46dba4799420838ce1425ad472eb
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-04-05 08:12:41 +0200

    Limpieza de comentarios reescritura de puntuacion

--- src/main/java/main/User.java
+++ src/main/java/main/User.java
@@ -8,6 +8,9 @@ public class User {
 
     static String archivoUsuarios = "users.txt";
     static String archivoUsuariosTemporal = "temp.txt";
+
+    File archivoOriginal = new File(archivoUsuarios);
+    File archivoTemporal = new File(archivoUsuariosTemporal);
     
     public static String nombre;
     public static Integer puntuacion;
@@ -73,37 +76,28 @@ public class User {
     }
 
     //Método que actualiza la puntuación del jugador en el archivo
-    public static void reescribirPuntuacion(String nombreUsuario, int nuevaPuntuacion) throws IOException{
+    public static void reescribirPuntuacion(String nombreUsuario, int nuevaPuntuacion) throws IOException {
 
         File archivoOriginal = new File(archivoUsuarios);
         File archivoTemporal = new File(archivoUsuariosTemporal);
 
         try (BufferedReader br = new BufferedReader(new FileReader(archivoOriginal));
              BufferedWriter bw = new BufferedWriter(new FileWriter(archivoTemporal))) {
-
             String linea;
-
             while ((linea = br.readLine()) != null) {
-                // Dividir la línea en nombre y puntuación utilizando el punto y coma como delimitador
                 String[] partes = linea.split(";");
                 if (partes.length == 2 && partes[0].equals(nombreUsuario)) {
-                    // Si se encuentra el usuario, actualizar la puntuación
                     linea = nombreUsuario + ";" + nuevaPuntuacion;
                 }
-                // Escribir la línea en el archivo temporal
                 bw.write(linea);
-                // Escribir un salto de línea después de cada línea
                 bw.newLine();
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if (archivoOriginal.exists()) {
-            archivoOriginal.delete();
-            archivoTemporal.renameTo(archivoOriginal);
-        }
+        archivoOriginal.delete();
+        archivoTemporal.renameTo(archivoOriginal);
     }
     
 }

b6f37002e17ba59a8c8255d675ce942605015d96
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-04-12 14:23:47 +0200

    Comportamiento completado

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,8 +10,13 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        User jugador = new User(establecerUsuario());
-        inicio(jugador);
+        //User jugador = new User(establecerUsuario());
+        //inicio(jugador);
+       
+        String respuesta = formatoJugador("hitler");
+        
+        System.out.println("El usuario es "+respuesta);
+
     }
 
     // Método que establece el jugador
@@ -43,8 +48,12 @@ public class Main {
 
     // Método que arregla el formato del user escrito para evitar repeticiones
     public static String formatoJugador(String jugador) {
-        jugador.toLowerCase();
-        return jugador;
+        
+        String primeraLetraMayuscula = jugador.substring(0, 1).toUpperCase();
+        String restoMinuscula = jugador.substring(1).toLowerCase();
+        
+        // Combinar la primera letra mayúscula con el resto en minúscula
+        return primeraLetraMayuscula + restoMinuscula;
     }
     
 }

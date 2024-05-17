e8e33c6d4d9f8d588e4e8f37c5dfdf70887f1cb9
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-05-17 09:49:02 +0200

    Método para mostrar jugadores completado

--- src/main/java/main/ControladorUsuarios.java
+++ src/main/java/main/ControladorUsuarios.java
@@ -1,11 +1,32 @@
 package main;
 
+import java.io.*;
+import static tools.Textos.*;
+
+
 public class ControladorUsuarios {
+
+    public static String linea;
+    static File archivoOriginal = new File(archivoUsuarios);
+    static File archivoTemporal = new File(archivoUsuariosTemporal);
 
     public ControladorUsuarios() {
         System.out.println("Indique qué desea realizar:");
+        System.out.println("Método en construccion. Le mostramos una lista de los jugadores disponibles.");
+        mostrarJugadores();
+    }
 
-        System.out.println("Método en construccion");
+    public void mostrarJugadores(){
+        try (BufferedReader br = new BufferedReader(new FileReader(archivoOriginal))) {
+            while ((linea = br.readLine()) != null) {
+                if(linea.length() > 1) {
+                    String[] partes = linea.split(separadorUsuarios);
+                    System.out.println("Nombre: "+partes[0]+". Puntuación: "+partes[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }

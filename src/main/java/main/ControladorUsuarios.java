2ae0cd749b11ef464148e7cee310974f1c3194d1
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-05-17 09:31:26 +0200

    Creación del constructor de ControladorUsuarios

--- src/main/java/main/ControladorUsuarios.java
+++ src/main/java/main/ControladorUsuarios.java
@@ -1,5 +1,11 @@
 package main;
 
 public class ControladorUsuarios {
+
+    public ControladorUsuarios() {
+        System.out.println("Indique qué desea realizar:");
+
+        System.out.println("Método en construccion");
+    }
     
 }

11e49e46c106192df24f844968bfe6fa65dba7fa
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-06-20 10:59:29 +0200

    Sacadas las lineas de codigo repetidas de comprobar puntuacion para crear un comportamiento aparte

--- src/test/app/UserTest.java
+++ src/test/app/UserTest.java
@@ -11,57 +11,49 @@ import static tools.Textos.separadorUsuarios;
 
 class UserTest {
 
+    String jugadorTestExistente = "playerTest060782";
+    String jugadorTestNoExistente = "playerTest220694";
+
     @Test
     void UsuarioExiste_True_FuncionaCorrectamente() {
-        String jugadorTest = "playerTest060782";
-
-        boolean jugadorExiste = usuarioExiste(jugadorTest);
-
+        boolean jugadorExiste = usuarioExiste(jugadorTestExistente);
         assertTrue(jugadorExiste, "El jugador comprobado no se encuentra en la BDD.");
     }
 
     @Test
     void UsuarioExiste_False_FuncionaCorrectamente() {
-        String jugadorTest = "playerTest220694";
-
-        boolean jugadorExiste = usuarioExiste(jugadorTest);
-
+        boolean jugadorExiste = usuarioExiste(jugadorTestNoExistente);
         assertFalse(jugadorExiste, "El jugador comprobado sí aparece en la BDD.");
     }
 
     @Test
     void ReescribirPuntuacion_FuncionaCorrectamente() {
-        String jugadorTest = "playerTest060782";
         int antiguaPuntuacion = 50;
         int nuevaPuntuacion = 30;
-        int puntuacionReal = 0;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(archivoOriginal))) {
-            while ((linea = br.readLine()) != null) {
-                String[] partes = linea.split(separadorUsuarios);
-                if (!linea.trim().isEmpty()) {
-                    if (partes.length == 2 && partes[0].equals(jugadorTest)) puntuacionReal = Integer.parseInt(partes[1]);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        int puntuacionReal = comprobarPuntuacion();
         assertEquals(antiguaPuntuacion,puntuacionReal,"La puntuación recibida no coincide con la esperada");
 
-        reescribirPuntuacion(jugadorTest,nuevaPuntuacion);
+        reescribirPuntuacion(jugadorTestExistente,nuevaPuntuacion);
+        puntuacionReal = comprobarPuntuacion();
+        assertEquals(nuevaPuntuacion,puntuacionReal,"La puntuación recibida no coincide con la esperada");
+
+        reescribirPuntuacion(jugadorTestExistente, antiguaPuntuacion);
+    }
+
+    int comprobarPuntuacion() {
+        int puntuacionReal = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(archivoOriginal))) {
             while ((linea = br.readLine()) != null) {
                 String[] partes = linea.split(separadorUsuarios);
                 if (!linea.trim().isEmpty()) {
-                    if (partes.length == 2 && partes[0].equals(jugadorTest)) puntuacionReal = Integer.parseInt(partes[1]);
+                    if (partes.length == 2 && partes[0].equals(jugadorTestExistente)) puntuacionReal = Integer.parseInt(partes[1]);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        assertEquals(nuevaPuntuacion,puntuacionReal,"La puntuación recibida no coincide con la esperada");
-
-        reescribirPuntuacion(jugadorTest, antiguaPuntuacion);
+        return puntuacionReal;
     }
 
 }

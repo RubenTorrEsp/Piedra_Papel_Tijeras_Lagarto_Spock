12af168b23c1aa53a8dc90d1c57809808ab74acb
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-06-24 09:19:22 +0200

    Creación de los primeros textos

--- src/test/tools/Texts.java
+++ src/test/tools/Texts.java
@@ -1,4 +1,9 @@
 package tools;
 
 public class Texts {
+
+    String mensajeErrorJugadorNoEnBDD = "El jugador comprobado no se encuentra en la BDD.";
+    String mensajeErrorJugadorYaEnBDD = "El jugador comprobado sí aparece en la BDD.";
+    String mensajeErrorPuntuacionNoCoincide = "La puntuación recibida no coincide con la esperada";
+
 }

963fe1bc28733ab2c7179ef524a7736e248490c6
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-06-24 09:21:07 +0200

    Modificacion de textos, modificadores public y static

--- src/test/tools/Texts.java
+++ src/test/tools/Texts.java
@@ -2,8 +2,8 @@ package tools;
 
 public class Texts {
 
-    String mensajeErrorJugadorNoEnBDD = "El jugador comprobado no se encuentra en la BDD.";
-    String mensajeErrorJugadorYaEnBDD = "El jugador comprobado sí aparece en la BDD.";
-    String mensajeErrorPuntuacionNoCoincide = "La puntuación recibida no coincide con la esperada";
+    public static String mensajeErrorJugadorNoEnBDD = "El jugador comprobado no se encuentra en la BDD.";
+    public static String mensajeErrorJugadorYaEnBDD = "El jugador comprobado sí aparece en la BDD.";
+    public static String mensajeErrorPuntuacionNoCoincide = "La puntuación recibida no coincide con la esperada";
 
 }

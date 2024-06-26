a611e233e8fd8bd4bd85ddcd53c09b93652b847d
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-06-26 09:18:30 +0200

    Eliminacion de conversores de parametros redundantes

--- src/main/java/app/JuegoClasico.java
+++ src/main/java/app/JuegoClasico.java
@@ -40,7 +40,6 @@ public class JuegoClasico extends Common{
         }
         else {
             System.out.println(seleccionInvalida);
-            seleccionValida = false;
         }
         return seleccionValida;
     }

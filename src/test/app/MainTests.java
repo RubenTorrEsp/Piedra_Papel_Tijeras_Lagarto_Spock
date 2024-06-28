f84efca21479d5ff45d2e2291d8c4fec732895ba
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-06-28 08:30:31 +0200

    Creacion del test FormatoJugador

--- src/test/app/MainTests.java
+++ src/test/app/MainTests.java
@@ -2,12 +2,20 @@ package app;
 
 import org.junit.jupiter.api.Test;
 
+import static app._Main.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class MainTests {
 
+    String nombreSinFormato = "user_Test";
+
     @Test
     void FormatoJugador_FuncionaCorrectamente() {
+        String nombreEsperado = "User_test";
+        String nombreTrasFormatear = formatoJugador(nombreSinFormato);
 
+        assertEquals(nombreEsperado,nombreTrasFormatear);
     }
 
 }

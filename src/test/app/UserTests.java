5d496b8eb1c73dc38536ae17c6bad83c6350209e
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-07-23 08:21:47 +0200

    Actualizacion de los ToDos

--- src/test/app/UserTests.java
+++ src/test/app/UserTests.java
@@ -45,7 +45,7 @@ class UserTests {
         reescribirPuntuacion(jugadorTestExistente, puntuacionInicial, archivoOriginalTests, archivoTemporalTests);
     }
 
-    //TODO: El objeto User reescribe el archivo de usuarios. Pendiente de corregir
+    //TODO: El objeto User ya no reescribe, pero no se crea el user. Revisar
     @Test
     void ActualizarPuntuacion_Victoria_FuncionaCorrectamente() {
         User jugadorTest = new User(jugadorTestExistente, archivoOriginalTests);
@@ -59,7 +59,7 @@ class UserTests {
         assertEquals(puntuacionTrasVictoria,puntuacionReal);
     }
 
-    //TODO: El objeto User reescribe el archivo de usuarios. Pendiente de corregir
+    //TODO: El objeto User ya no reescribe, pero no se crea el user. Revisar
     @Test
     void ActualizarPuntuacion_Derrota_FuncionaCorrectamente() {
         User jugadorTest = new User(jugadorTestExistente, archivoOriginalTests);

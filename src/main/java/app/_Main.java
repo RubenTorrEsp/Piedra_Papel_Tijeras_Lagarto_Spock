aed0dfa32e6e08168a65a8c1123c2f19060b0173
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-07-23 08:13:58 +0200

    Constructor de Inicio, archivo enviado como parámetro en el switch-case a la hora de construir el User

--- src/main/java/app/_Main.java
+++ src/main/java/app/_Main.java
@@ -3,6 +3,7 @@ package app;
 import static app.Common.*;
 import static resources.Textos.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -10,6 +11,7 @@ public class _Main {
     
     static Scanner scanner = new Scanner(System.in);
     static User jugador;
+    static File archivoOriginal = new File(archivoUsuarios);
 
     public static void main(String[] args) throws IOException {
         inicio();
@@ -20,11 +22,11 @@ public class _Main {
         String eleccion = scanner.nextLine();
         switch (eleccion) {
             case seleccionNumero1:
-                jugador = new User(establecerUsuario());
+                jugador = new User(establecerUsuario(), archivoOriginal);
                 while(Common.reJugar) new JuegoExtendido(jugador);
                 break;
             case seleccionNumero2:
-                jugador = new User(establecerUsuario());    
+                jugador = new User(establecerUsuario(), archivoOriginal);
                 while(Common.reJugar) new JuegoClasico(jugador);
                 break;
             case seleccionNumero9:

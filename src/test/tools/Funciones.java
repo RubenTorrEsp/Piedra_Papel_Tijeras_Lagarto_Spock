9b5d2d357041ef4a85f31b233b5cf786500d53b9
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-08-06 08:24:43 +0200

    Traslado de la función comprobarPuntuacion

--- src/test/tools/Funciones.java
+++ src/test/tools/Funciones.java
@@ -1,4 +1,27 @@
 package tools;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static app.User.linea;
+import static resources.Textos.separadorUsuarios;
+
 public class Funciones {
+    public int comprobarPuntuacion(String jugador, File file) {
+        int puntuacionReal = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(separadorUsuarios);
+                if (!linea.trim().isEmpty()) {
+                    if (partes.length == 2 && partes[0].equals(jugador)) puntuacionReal = Integer.parseInt(partes[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return puntuacionReal;
+    }
+
 }

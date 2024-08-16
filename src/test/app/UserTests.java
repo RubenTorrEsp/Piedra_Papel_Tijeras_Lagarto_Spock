583b2a8c0b403490841c34018a03c51cd7f07c5a
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-08-16 19:24:19 +0200

    Test ReescribirPuntuacion corregido

--- src/test/app/UserTests.java
+++ src/test/app/UserTests.java
@@ -35,7 +35,7 @@ class UserTests {
         assertEquals(puntuacionInicial, comprobarPuntuacion(jugadorTestExistente, testUsersFile), mensajeErrorPuntuacionNoCoincide);
 
         reescribirPuntuacion(jugadorTestExistente, 30, testUsersFile, tempFile);
-        assertEquals(nuevaPuntuacion, comprobarPuntuacion(jugadorTestExistente, testUsersFile), mensajeErrorPuntuacionNoCoincide);
+        assertEquals(30, comprobarPuntuacion(jugadorTestExistente, testUsersFile), mensajeErrorPuntuacionNoCoincide);
         reescribirPuntuacion(jugadorTestExistente, puntuacionInicial, testUsersFile, tempFile);
     }
 

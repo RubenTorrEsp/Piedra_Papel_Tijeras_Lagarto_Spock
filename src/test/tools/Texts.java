94cee02c93dec6c9c2c252053117964cacf7c28f
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-08-27 12:56:06 +0200

    Actualizacion textos

--- src/test/tools/Texts.java
+++ src/test/tools/Texts.java
@@ -2,6 +2,14 @@ package tools;
 
 public class Texts {
 
+    // Jugadores
+    public static String jugadorTest = "playerTest220694";
+    public static String jugadorTestParaModificar = "playerTest251289";
+    public static String jugadorTestParaReiniciar = "playerTest090880";
+    public static String jugadorNuevoNombre = "playerTestNewName";
+    public static String jugadorTestNoExistente = "playerNoExistente";
+
+    // Mensajes de error
     public static String mensajeErrorJugadorNoEnBDD = "El jugador comprobado no se encuentra en la BDD.";
     public static String mensajeErrorJugadorYaEnBDD = "El jugador comprobado sí aparece en la BDD.";
     public static String mensajeErrorPuntuacionNoCoincide = "La puntuación recibida no coincide con la esperada";

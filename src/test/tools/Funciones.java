6c754dbfcc2362f6f9e17a3fd49b8fad910e032c
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-08-28 08:17:49 +0200

    Mejora de la eficiencia de la funcion comprobarPuntuacion

--- src/test/tools/Funciones.java
+++ src/test/tools/Funciones.java
@@ -14,16 +14,24 @@ public class Funciones extends Texts {
 
     public static int comprobarPuntuacion(String jugador, File file) {
         int puntuacionReal = 0;
+
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String linea;
+
             while ((linea = br.readLine()) != null) {
                 String[] partes = linea.split(separadorUsuarios);
-                if (!linea.trim().isEmpty()) {
-                    if (partes.length == 2 && partes[0].equals(jugador)) puntuacionReal = Integer.parseInt(partes[1]);
+
+                if (partes.length == 2 && partes[0].equals(jugador)) {
+                    puntuacionReal = Integer.parseInt(partes[1].trim());
+                    break; // Salimos del bucle una vez encontrada la puntuación del jugador
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Formato de puntuación inválido para el jugador " + jugador + ": " + e.getMessage());
         }
+
         return puntuacionReal;
     }
 

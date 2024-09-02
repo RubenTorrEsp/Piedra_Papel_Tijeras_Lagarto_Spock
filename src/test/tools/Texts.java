62a2da1bc21d6b3a3880168bffcd8a9f5d6f4e5a
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-09-02 09:15:08 +0200

    Actualizaion de los textos

--- src/test/tools/Texts.java
+++ src/test/tools/Texts.java
@@ -4,14 +4,15 @@ public class Texts {
 
     // Archivos
     public static final String archivoUsuariosTest = "src\\test\\resources\\userTest.txt";
+    public static final String archivoUsuariosTestTemplate = "src\\test\\resources\\userTestTemplate.txt";
     public static final String archivoUsuariosTemporalTest = "src\\test\\resources\\tempTest.txt";
 
     // Jugadores
+    public String jugadorTestExistente = "playerTest060782";
     public String jugadorTest = "playerTest220694";
     public String jugadorTestParaModificar = "playerTest251289";
     public String jugadorTestParaReiniciar = "playerTest090880";
     public String jugadorNuevoNombre = "playerTestNewName";
-    public String jugadorTestExistente = "playerTest060782";
     public String jugadorTestNoExistente = "playerNoExistente";
 
     // Mensajes de error

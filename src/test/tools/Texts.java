7386f8e3a1e9ca6c8d4ddd1ab931625ee1f21630
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-09-11 11:16:24 +0200

    Valores final en formato estandarizado

--- src/test/tools/Texts.java
+++ src/test/tools/Texts.java
@@ -4,32 +4,32 @@ public class Texts {
 
     // Archivos
     public static final String archivoUsuariosTest = "src\\test\\resources\\userTest.txt";
-    public static final String archivoUsuariosTestTemplate = "src\\test\\resources\\userTestTemplate.txt";
+    public final String archivoUsuariosTestTemplate = "src\\test\\resources\\userTestTemplate.txt";
     public static final String archivoUsuariosTemporalTest = "src\\test\\resources\\tempTest.txt";
 
     // Jugadores
-    public static final String jugadorTestExistente = "playerTest060782";
-    public static final String jugadorTest = "playerTest220694";
-    public static final String jugadorTestParaCrear = "playerTest200995";
-    public static final String jugadorTestParaModificar = "playerTest251289";
-    public static final String jugadorTestParaReiniciar = "playerTest090880";
-    public static final String jugadorNuevoNombre = "playerTestNewName";
-    public static final String jugadorTestNoExistente = "playerNoExistente";
-    public static final String jugadorSinFormato = "user_Test";
+    public final String jugadorTestExistente = "playerTest060782";
+    public final String jugadorTest = "playerTest220694";
+    public final String jugadorTestParaCrear = "playerTest200995";
+    public final String jugadorTestParaModificar = "playerTest251289";
+    public final String jugadorTestParaReiniciar = "playerTest090880";
+    public final String jugadorNuevoNombre = "playerTestNewName";
+    public final String jugadorTestNoExistente = "playerNoExistente";
+    public final String jugadorSinFormato = "user_Test";
 
     // Puntuaciones
-    public static final int puntuacionInicial = 50;
-    public static final int puntuacionTrasVictoria = 51;
-    public static final int puntuacionTrasDerrota = 49;
-    public static final int nuevaPuntuacion = 30;
+    public final int puntuacionInicial = 50;
+    public final int puntuacionTrasVictoria = 51;
+    public final int puntuacionTrasDerrota = 49;
+    public final int nuevaPuntuacion = 30;
 
     // Comportamientos
-    public static final boolean victoria = true;
-    public static final boolean derrota = false;
+    public final boolean VICTORY = true;
+    public final boolean DEFEAT = false;
 
     // Mensajes de error
-    public static final String PLATER_NOT_IN_DB = "El jugador comprobado no se encuentra en la BDD.";
-    public static final String PLAYER_ALREADY_IN_DB = "El jugador comprobado sí aparece en la BDD.";
-    public static final String SCORE_NOT_MATCH = "La puntuación recibida no coincide con la esperada";
+    public final String PLATER_NOT_IN_DB = "El jugador comprobado no se encuentra en la BDD.";
+    public final String PLAYER_ALREADY_IN_DB = "El jugador comprobado sí aparece en la BDD.";
+    public final String SCORE_DOES_NOT_MATCH = "La puntuación recibida no coincide con la esperada";
 
 }

561b0a80411d9180a0654e9c22e4f2cd9e6a3079
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-09-23 09:17:00 +0200

    Corrección de los valores de textos

--- src/main/java/app/JuegoClasico.java
+++ src/main/java/app/JuegoClasico.java
@@ -21,7 +21,7 @@ public class JuegoClasico extends Common{
     public void jugarDeNuevo(User jugador) {
         System.out.println(opcionesJuegoClasico);
         if(obtenerEntrada(jugador)){
-            if (Objects.equals(seleccionJugador, seleccionSalir)) reJugar = false;
+            if (Objects.equals(seleccionJugador, SELECT_OUT)) reJugar = false;
             else {
                 comprobarVictoria(seleccionJugador, obtenerSeleccionMaquina(3), jugador);
                 reJugar();

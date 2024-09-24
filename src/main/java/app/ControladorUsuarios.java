c93951477216b76724d124d2bb075fec2e7fed20
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-09-24 11:39:33 +0200

    Arreglo de la importación por la refactorizacion de la clase Texts

--- src/main/java/app/ControladorUsuarios.java
+++ src/main/java/app/ControladorUsuarios.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 import static app.Common.*;
 import static app.User.*;
-import static resources.Textos.*;
+import static resources.Texts.*;
 
 public class ControladorUsuarios {
 

37d2829e937716e1190952ff49cbbbef112d0f19
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-12-11 21:24:16 +0100

    Refactorizacion de los textos

--- src/main/java/resources/Texts.java
+++ src/main/java/resources/Texts.java
@@ -100,7 +100,7 @@ public class Texts {
 
         // Mostrar jugadores
         public static final String SHOW_PLAYERS = "Aquí se muestran los jugadores disponibles";
-        public static void showPlayer(String name, String puntuacion) {
+        public static void showPlayer(String name, String score) {
          System.out.println(STR."Nombre: \{nombre}. Puntuación: \{puntuacion}");
         }
         // Crear jugadores

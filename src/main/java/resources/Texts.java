2abdb7f202b5d615b74305c72836b57150210d84
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-12-13 09:11:17 +0100

    Refactorizacion de los textos

--- src/main/java/resources/Texts.java
+++ src/main/java/resources/Texts.java
@@ -105,7 +105,7 @@ public class Texts {
         }
         // Crear jugadores
         public static final String PLAYER_CREATE = "Indique el jugador que desea crear";
-        public static void indicarJugadorCreado(String playerCreated) {
+        public static void indicatePlayerCreated(String playerCreated) {
             System.out.println(STR."El jugador \{playerCreated} ha sido añadido a la base de datos");
         }
         public static void indicarJugadorYaEnBDD(String playerInDB) {

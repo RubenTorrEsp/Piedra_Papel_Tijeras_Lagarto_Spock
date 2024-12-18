8184c0190b4875fa65acd45dacfb210b291e8359
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-12-18 12:30:30 +0100

    Refactorizacion de los textos

--- src/main/java/resources/Texts.java
+++ src/main/java/resources/Texts.java
@@ -126,7 +126,7 @@ public class Texts {
             System.out.println(STR."El jugador \{playerReStarted} vuelve a tener ahora 50 puntos.");
         }
         //Podium
-        public static void podium(List<List<Object>> jugadores) {
+        public static void podium(List<List<Object>> players) {
             System.out.println("Medalla de ORO para...");
             System.out.println(STR."El jugador \"\{jugadores.getFirst().getFirst()}\" con \{jugadores.getFirst().getLast()} puntos");
             System.out.println("Medalla de PLATA para...");

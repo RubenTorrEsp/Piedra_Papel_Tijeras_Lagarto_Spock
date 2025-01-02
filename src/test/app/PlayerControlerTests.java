83c4e5c1ae8b9ad318c292b388e9daf832b9fe8f
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2025-01-02 08:20:15 +0100

    Refactorizacion de los tests

--- src/test/app/PlayerControlerTests.java
+++ src/test/app/PlayerControlerTests.java
@@ -39,7 +39,7 @@ public class PlayerControlerTests extends Functions {
 
     @Test
     void modificarJugador_jugadorExistente() throws IOException {
-        assertTrue(userExists(PLAYER_MODIFY, archivoOriginalTests));
+        assertTrue(userExists(PLAYER_MODIFY, originalFileTests));
         assertFalse(userExists(PLAYER_NEW_NAME, archivoOriginalTests));
 
         modifyPlayer(

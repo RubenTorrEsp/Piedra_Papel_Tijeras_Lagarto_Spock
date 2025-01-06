ce20fbd81c3d93454e3639dbb3363290a52e8724
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2025-01-06 23:08:33 +0100

    Refactorizacion de los tests

--- src/test/app/PlayerControlerTests.java
+++ src/test/app/PlayerControlerTests.java
@@ -65,7 +65,7 @@ public class PlayerControlerTests extends Functions {
     void eliminarJugador_jugadorNoExistente() throws IOException {
         int jugadoresIniciales = countPlayersInDB(originalFileTests);
 
-        PlayerControler.deletePlayer(Optional.of(PLAYER_DELETE_FALSE), archivoOriginalTests, archivoTemporalTests);
+        PlayerControler.deletePlayer(Optional.of(PLAYER_DELETE_FALSE), originalFileTests, archivoTemporalTests);
         int jugadoresTrasEliminacion = countPlayersInDB(archivoOriginalTests);
 
         assertEquals(jugadoresIniciales, jugadoresTrasEliminacion);

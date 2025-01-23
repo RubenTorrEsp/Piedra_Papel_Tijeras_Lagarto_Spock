3de632c228cc3110fc6c204e0f566eefa17397a2
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2025-01-23 08:18:15 +0100

    Arreglo de los tests, para que no se quede esperando respuesta para ejecutar el controlador de usuarios

--- src/test/app/PlayerControlerTests.java
+++ src/test/app/PlayerControlerTests.java
@@ -88,8 +88,6 @@ public class PlayerControlerTests extends Functions {
         String simulatedInput = "no\n";
         ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
         System.setIn(inputStream);
-
-        String response = "No";
     }
 
 }
